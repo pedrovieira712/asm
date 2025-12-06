b2d422278d22298de95bf9c7c0d8a828fcc766ef
a104181 <a104181@alunos.uminho.pt>
2025-12-06 10:05:02 +0000

    add: Vehicle, CentralManager, Location

---
+++ Agents/Vehicle/Behaviours/Vc_behav.py
@@ -0,0 +1,9 @@
+from spade.behaviour import CyclicBehaviour, OneShotBehaviour, PeriodicBehaviour
+from spade.message import Message
+import jsonpickle
+
+class SendFowardingRequestBehaviour(OneShotBehaviour):
+    pass
+
+class ReceiveFowardingResponseBehaviour(OneShotBehaviour):
+    pass
